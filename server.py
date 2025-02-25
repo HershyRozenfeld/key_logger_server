318cac86cdf9dd80f203650764d63e10a74ba08c
Hershy Rozenfeld <153819469+HershyRozenfeld@users.noreply.github.com>
2025-02-25 15:19:22 +0200

    Update server.py
    add CROS

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import os
 
 app = Flask(__name__)
+CORS(app)  # מאפשר בקשות מכל דומיין
 
 
 def write_to_json(name_j, data):
